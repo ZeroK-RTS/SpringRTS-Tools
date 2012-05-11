e881d58bd9dede1154f82775cfa1d8ef700de2f9
GoogleFrog <googl3frog@gmail.com>
2012-05-11 05:46:22 +0000

    Mex to Infolog with marker erasing.
    svn path=/trunk/tools/; revision=6481

--- lua/Widgets/dbg_mex_to_infolog.lua
+++ lua/Widgets/dbg_mex_to_infolog.lua
@@ -12,19 +12,24 @@ function widget:GetInfo()
 end
 	
 local metalSpotsNil = true
+local markerFrame = false
 	
 function widget:Update()
 	if metalSpotsNil and WG.metalSpots ~= nil then
+		local total = 0
 		if WG.metalSpots then
 			Spring.Echo("Metal Spots")
 			for i = 1, #WG.metalSpots do
 				local spot = WG.metalSpots[i]
 				Spring.Echo("{x = " .. spot.x .. ", z = " .. spot.z .. ", metal = " .. spot.metal .. "},")
+				total = total + spot.metal
 			end
 			for i = 1, #WG.metalSpots do
 				local spot = WG.metalSpots[i]
-				Spring.MarkerAddPoint(spot.x, 0, spot.z, i .. ": " .. spot.metal)
+				Spring.MarkerErasePosition(spot.x,0,spot.z)
 			end
+			markerFrame = Spring.GetGameFrame()+30
+			Spring.Echo("Total Spot Metal " .. total)
 		else
 			Spring.Echo("Invalid Metal Map")
 		end
@@ -32,3 +37,12 @@ function widget:Update()
 	end
 end
 	
+	
+function widget:GameFrame(f)
+	if markerFrame == f then
+		for i = 1, #WG.metalSpots do
+			local spot = WG.metalSpots[i]
+			Spring.MarkerAddPoint(spot.x, 0, spot.z, i .. ": " .. spot.metal)
+		end
+	end
+end
